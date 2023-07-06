b061c865b848ee8ece4bd63a4630b402bab6e34c
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-07-06 09:53:59 +0500

    solution 35 binary search

--- lib/solution_35.dart
+++ lib/solution_35.dart
@@ -1,6 +1,16 @@
 ///Search Insert Position
-class Solution {
+class Solution35 {
   int searchInsert(List<int> nums, int target) {
-    return 0;
+    int left = 0;
+    int right = nums.length;
+    while (left < right) {
+      int mid = (left + right) ~/ 2;
+      if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return left;
   }
 }
