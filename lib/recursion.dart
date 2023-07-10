833e20fbefbbcab7cd16357e8ef80d4b7cb8b9d9
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-07-10 11:09:55 +0500

    solution 2733

--- lib/recursion.dart
+++ lib/recursion.dart
@@ -1,4 +1,13 @@
+int factorialWithRecursion(int n) {
 
+  if (n == 1) {
+    return 1;
+  } else {
+    return n * factorialWithRecursion(n = 1);
+  }
+  ///second way
+  ///return n == 1 ? 1 : n * factorial(n - 1);
+}
 int factorial(int n) {
   int k = 1;
   while (n > 0) {
