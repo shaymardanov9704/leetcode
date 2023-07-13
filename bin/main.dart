4b316db4b9a82a51ebf328cd29ab122c31b21551
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-07-13 11:45:08 +0500

    solution 3

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,5 @@
-import 'package:leetcode_trainings/buble_sort.dart';
-import 'package:leetcode_trainings/merge_sort.dart';
-import 'package:leetcode_trainings/quick_sort.dart';
+import 'package:leetcode_trainings/solution_3.dart';
 
 void main() {
-  print(quickSort([1, 2, 3, 4, 5, 6, 3, 11, 32, 41, 15, 6]));
-  print(bubbleSort([1, 2, 3, 4, 5, 6, 3, 11, 32, 41, 15, 6]));
-  print(mergeSort([1, 2, 3, 4, 5, 6, 3, 11, 32, 41, 15, 6]));
+  Solution3().lengthOfLongestSubstring("abcabcsss");
 }
--- lib/solution_3.dart
+++ lib/solution_3.dart
@@ -1,6 +1,44 @@
 class Solution3 {
   int lengthOfLongestSubstring(String s) {
-    s = "abcabcbb";
-    return 3;
+    if (s.isEmpty) {
+      return 0;
+    }
+    Set<String> subString = {};
+    int longestSubString = 1;
+    for (int i = 0; i < s.length; i++) {
+      print(subString);
+      while (subString.contains(s[i])) {
+        subString.remove(subString.first);
+      }
+      subString.add(s[i]);
+      print(subString);
+      if (longestSubString < subString.length) {
+        longestSubString = subString.length;
+      }
+      print(longestSubString);
+    }
+    return longestSubString;
   }
+
+// int lengthOfLongestSubstring(String s) {
+//   List<int> substrings = [];
+//   for (int i = 1; i < s.length - 1; i++) {
+//     isUnical(s.substring(i)) ? substrings.add(s.substring(i).length) : null;
+//   }
+//   substrings.sort();
+//   print(substrings);
+//   return 1;
+// }
+//
+// bool isUnical(String str) {
+//   bool result = false;
+//   Set<String> b = str.split("").toSet();
+//   final s = b.join();
+//   if (str.contains(s)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+//
+// }
 }
