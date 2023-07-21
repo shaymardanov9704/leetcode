814de6ce8ab911d59e009e2ffccf9fe95d356467
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-07-21 17:48:27 +0500

    fix

--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,1 @@
-void main() {
-  int distanceTraveled(int mainTank, int additionalTank) {
-    int distance = 0;
-    int steps = 0;
-    while (mainTank > 0) {
-      mainTank--;
-      steps++;
-      distance += 10;
-      if (steps % 5 == 0 && additionalTank > 0) {
-        mainTank++;
-        additionalTank--;
-      }
-    }
-    return distance;
-  }
-
-  distanceTraveled(9, 1);
-}
+void main() {}
