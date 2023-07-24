c7317506874d0f5ae73e848023f63d666c60e12b
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-07-24 09:55:00 +0500

    solution 2729 Check if The Number is Fascinating

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,8 @@
-void main() {}
+void main() {
+  bool isFascinating(int n) {
+    List s = "$n${n * 2}${3 * n}".split("");
+    return s.length == 9 && !s.contains("0") && s.toSet().length == 9;
+  }
+
+  print(isFascinating(384));
+}
---
+++ lib/solution_2729.dart
@@ -0,0 +1,9 @@
+/// Check if The Number is Fascinating
+
+class Solution {
+  bool isFascinating(int n) {
+    n = n * 100000 + n * 2000 + 3 * n;
+    print(n);
+    return false;
+  }
+}
