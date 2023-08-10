8a5e1ec3aea8aa7ffa857dfbf459f08853bd5270
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-08-10 17:29:28 +0500

    solution 15 Three Sum

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,25 @@
-void main() {}
+void main() {
+  Map<String, List<int>> result = {};
+  var buf = <int>[];
+  List<List<int>> threeSum(List<int> nums) {
+    nums.sort();
+    for (int k = 0; k < nums.length - 2; k++) {
+      if (nums[k] > 0) break;
+      if (k > 0 && nums[k] == nums[k - 1]) continue;
+      int j = k + 1, i = nums.length - 1;
+      while (j < i) {
+        if (nums[i] < 0) break;
+        if (nums[k] + nums[j] + nums[i] == 0) {
+          buf = [nums[k], nums[j], nums[i]];
+          result[buf.toString()] = buf;
+          j++;
+        } else {
+          nums[k] + nums[j] + nums[i] > 0 ? i-- : j++;
+        }
+      }
+    }
+    return result.values.toList();
+  }
+
+  threeSum([-1, 0, 1, 2, -1, -4]);
+}
--- lib/solution_15.dart
+++ lib/solution_15.dart
@@ -1,15 +1,33 @@
-/// 3Sum
+/// ThreeSum
 
 class Solution15 {
   List<List<int>> threeSum(List<int> nums) {
-    List<List<int>> list = [];
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i; j < nums.length - 1; j++) {
-        if (nums[i] + nums[j] + nums[j + 1] == 0) {
-          list.add([nums[i], nums[j], nums[j + 1]]);
+    nums.sort();
+    List<List<int>> result = [];
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+        int left = i + 1;
+        int right = nums.length - 1;
+        int target = -nums[i];
+        while (left < right) {
+          if (nums[left] + nums[right] == target) {
+            result.add([nums[i], nums[left], nums[right]]);
+            while (left < right && nums[left] == nums[left + 1]) {
+              left++;
+            }
+            while (left < right && nums[right] == nums[right - 1]) {
+              right--;
+            }
+            left++;
+            right--;
+          } else if (nums[left] + nums[right] < target) {
+            left++;
+          } else {
+            right--;
+          }
         }
       }
     }
-    return list;
+    return result;
   }
 }
