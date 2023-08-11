bc32bdb80e69eaba97d17a144fdae2089490a1ac
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-08-11 10:04:39 +0500

    solution 39 Combination Sum

---
+++ lib/solution_39.dart
@@ -0,0 +1,25 @@
+///Combination Sum
+
+class Solution {
+  List<List<int>> combinationSum(List<int> candidates, int target) {
+    List<List<int>> result = [];
+    solve(0, candidates, target, result, []);
+    return result;
+  }
+
+  void solve(int i, List<int> candidates, int target, List<List<int>> result,
+      List<int> temp) {
+    if (i == candidates.length) {
+      if (target == 0) {
+        result.add(List.from(temp));
+      }
+      return;
+    }
+    if (candidates[i] <= target) {
+      temp.add(candidates[i]);
+      solve(i, candidates, target - candidates[i], result, temp);
+      temp.removeLast();
+    }
+    solve(i + 1, candidates, target, result, temp);
+  }
+}
